2b416d13dc08852f17c262e64a6f77085e72d8f8
wartinich <vasilenko.arhip@gmail.com>
2022-08-22 10:28:33 +0300

    Include user.urls

--- portal/urls.py
+++ portal/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/", include("authentication.urls")),
-    path("profile/", include("user_profile.urls")),
     path("assitance/", include("assistance.urls")),
-    path("petition/", include("petition.urls"))
+    path("petition/", include("petition.urls")),
+    path("profile/", include("user_profile.urls")),
+    path("user/", include("user.urls"))
 ]
