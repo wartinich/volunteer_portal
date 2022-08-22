2fbdb3b9d89c0343146fb81b2f0f9fd2376dc960
wartinich <vasilenko.arhip@gmail.com>
2022-08-22 10:12:45 +0300

    Add tests for login

--- authentication/tests.py
+++ authentication/tests.py
@@ -26,13 +26,3 @@ def test_password_not_valid(client, db, auth_data):
     auth_data["password"] = "wrong_password"
     resp = client.post(reverse("authentication:login"), data=auth_data)
     assert resp.status_code == 200
-
-
-def test_login_view(client):
-    resp = client.get(reverse("authentication:login"))
-    assert resp.status_code == 200
-
-
-def test_login_view_no_access(client):
-    resp = client.get(reverse("authentication:login"))
-    assert resp.status_code == 404
