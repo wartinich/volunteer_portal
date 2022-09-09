8d298ff37d693047ba1b29c20fc5f389d343d32f
wartinich <vasilenko.arhip@gmail.com>
2022-09-09 08:51:49 +0300

    Make migrations for follower

--- follower/models.py
+++ follower/models.py
@@ -4,8 +4,8 @@ from authentication.models import User
 
 
 class Follower(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="owner")
-    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="subscriber")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subscriber")
 
     class Meta:
         db_table = "followers"
