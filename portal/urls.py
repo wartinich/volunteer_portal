1350a89c40635a3c2d414a33cf36fa12e079074d
wartinich <vasilenko.arhip@gmail.com>
2022-09-11 10:37:31 +0300

    Add static and media

--- portal/urls.py
+++ portal/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from portal import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +13,7 @@ urlpatterns = [
     path("assitance/", include("assistance.urls")),
     path("petition/", include("petition.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
