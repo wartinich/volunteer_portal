45251a5083b5d796877cf90d37353a30d70d7d81
wartinich <vasilenko.arhip@gmail.com>
2022-09-17 08:50:45 +0300

    Add null for birth_date

--- authentication/models.py
+++ authentication/models.py
@@ -9,7 +9,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name="email", max_length=250, unique=True)
     first_name = models.CharField(verbose_name="first name", max_length=40)
     last_name = models.CharField(verbose_name="last name", max_length=80)
-    birth_date = models.DateField(verbose_name="birth date")
+    birth_date = models.DateField(verbose_name="birth date", null=True)
     user_photo = models.ImageField(verbose_name="user photo", upload_to="users/", null=True)
     bio = models.TextField(verbose_name="bio", max_length=150, null=True)
     is_active = models.BooleanField(verbose_name="is active", default=True)
