37639c5dde2568d995a9974d9415cffa89a2d75e
wartinich <vasilenko.arhip@gmail.com>
2022-09-24 16:55:40 +0300

    Add swagger for api

--- portal/urls.py
+++ portal/urls.py
@@ -1,19 +1,25 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
 
 from portal import settings
 
+schema_view = get_swagger_view(title='Volunteer portal API')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path("auth/", include("authentication.urls")),
+    path("auth/", include("social_django.urls"), name="social"),
     path("profile/", include("user_profile.urls")),
     path("follower/", include("follower.urls")),
     path("user/", include("user.urls")),
     path("assitance/", include("assistance.urls")),
     path("petition/", include("petition.urls")),
 
-    path("auth/", include('social_django.urls'), name="social"),
+    path("api/", include("api.urls")),
+    path("swagger/", schema_view),
 ]
 
 if settings.DEBUG:
