e1cb1eccae151a85725ec5470c3f1d1fe4adfad3
wartinich <vasilenko.arhip@gmail.com>
2022-10-15 12:23:06 +0300

    Refactor assistance statuses

--- assistance/constants.py
+++ assistance/constants.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class AssistanceStatus(Enum):
     ASSISTANCE_SETUP = "Setup"
-    ASSISTANCE_PAUSE = "PAUSE"
+    ASSISTANCE_PAUSE = "Pause"
     ASSISTANCE_IPS = "In progress"
     ASSISTANCE_CPD = "Completed"
